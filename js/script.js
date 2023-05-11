23750b89d0e5b480646a8ee15b0276e6599e465c
denist123 <denist2022@gmail.com>
2023-05-11 16:29:12 +0900

    Готова главная страница ПК

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-let header = () => {
-   const headerBurger = document.querySelector('.header__burger');
-   const headerMenu = document.querySelector('.header__menu');
-   headerBurger.addEventListener('click', () => {
-     if (headerBurger.classList.contains('_active')) {
-       headerBurger.classList.remove('_active');
-       headerMenu.style.width = '0';
-     } else {
-       headerBurger.classList.add('_active');
-       headerMenu.style.width = headerMenu.scrollWidth + 15 + 'px';
-     }
-   });
+// let header = () => {
+//    const headerBurger = document.querySelector('.header__burger');
+//    const headerMenu = document.querySelector('.header__menu');
 
-   const searchInput = document.getElementById('search');
-   window.addEventListener('resize', () => {
-   const windowWidth = window.innerWidth;
-   if (windowWidth < 500) {
-      searchInput.placeholder = "Артикул";
-   } else {
-      searchInput.placeholder = "Поиск по артикулу или VIN коду";
-   }
-   });
- };
- header();
+//    const searchInput = document.getElementById('search');
+//    window.addEventListener('resize', () => {
+//    const windowWidth = window.innerWidth;
+//    if (windowWidth < 500) {
+//       searchInput.placeholder = "Артикул";
+//    } else {
+//       searchInput.placeholder = "Поиск по артикулу или VIN коду";
+//    }
+//    });
+//  };
+//  header();
 
 
